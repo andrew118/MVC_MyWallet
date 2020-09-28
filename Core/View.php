e7a04910b6d0a2a1e9783aced7dfa18398a80051
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-09-28 12:33:02 +0200

    Authentication code moved to separate class

--- Core/View.php
+++ Core/View.php
@@ -24,7 +24,7 @@ class View
         if ($twig === null) {
             $loader = new \Twig\Loader\Filesystemloader(dirname(__DIR__) . '/App/Views');
             $twig = new \Twig\Environment($loader);
-			$twig->addGlobal('session', $_SESSION);
+			$twig->addGlobal('is_logged_in', \App\Auth::isLoggedIn());
         }
 
         echo $twig->render($template, $args);
