f1d700e5b88824a7ac9d75840874e96d5dc22f20
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-09-29 11:15:42 +0200

    Added controller that requires login for all actions

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -4,13 +4,8 @@ namespace App\Controllers;
 
 use \Core\View;
 
-class Items extends \Core\Controller
+class Items extends Authenticated
 {
-	protected function before()
-	{
-		$this->requireLogin();
-	}
-	
 	public function indexAction()
 	{
 		View::renderTemplate('Items/index.html');
