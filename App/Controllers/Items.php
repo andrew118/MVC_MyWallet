94b33a8e7c9c94c0a7380dafbb27f17881f34b3c
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-09-29 11:08:28 +0200

    Check user is logged in using before method filter

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -6,10 +6,18 @@ use \Core\View;
 
 class Items extends \Core\Controller
 {
-	public function indexAction()
+	protected function before()
 	{
 		$this->requireLogin();
-		
+	}
+	
+	public function indexAction()
+	{
 		View::renderTemplate('Items/index.html');
+	}
+	
+	public function newAction()
+	{
+		echo('Nowy wydatek');
 	}
 }
