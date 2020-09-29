e6560e1c2fecd1b11440872f6d3191a28abc19cf
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-09-29 11:42:48 +0200

    Get the current logged in user in controllers and views

--- App/Auth.php
+++ App/Auth.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Models\User;
+
 class Auth
 {
 	public static function login($user)
@@ -43,5 +45,12 @@ class Auth
 	public static function getReturnToPage()
 	{
 		return $_SESSION['return_to'] ?? '/';
+	}
+	
+	public static function getUser()
+	{
+		if (isset($_SESSION['user_id'])) {
+			return User::findByID($_SESSION['user_id']);
+		}
 	}
 }
--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Auth;
 
 class Home extends \Core\Controller
 {
