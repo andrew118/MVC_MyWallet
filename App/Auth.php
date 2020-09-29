406a097aad735a97f2720ec2a4104bfd8adbb8a4
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-09-29 17:29:57 +0200

    Login using cookie

--- App/Auth.php
+++ App/Auth.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Models\User;
+use App\Models\RememberedLogin;
 
 class Auth
 {
@@ -52,6 +53,25 @@ class Auth
 	{
 		if (isset($_SESSION['user_id'])) {
 			return User::findByID($_SESSION['user_id']);
+		} else {
+			return static::loginFromRememberCookie();
+		}
+	}
+	
+	protected static function loginFromRememberCookie()
+	{
+		$cookie = $_COOKIE['remember_me'] ?? false;
+		
+		if ($cookie) {
+			$remembered_login = RememberedLogin::findByToken($cookie);
+			
+			if ($remembered_login) {
+				$user = $remembered_login->getUser();
+				
+				static::login($user, false);
+				
+				return $user;
+			}
 		}
 	}
 }
