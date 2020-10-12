8fbf520dd677df3af469cbff51dabd64d3e7fa3c
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-10-12 22:28:25 +0200

    Flash messages for invalid date

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -5,13 +5,12 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\CashFlow;
 use \DateTime;
+use \App\Flash;
 
 class Balance extends Authenticated
 {
   private $beginDate;
   private $endDate;
-  
-  public $errors = [];
   
   public function showAction()
   {
@@ -36,14 +35,16 @@ class Balance extends Authenticated
       
     } else if (isset($_POST['begin_date'], $_POST['end_date'])) {
       
-      $this->validateDateRange($_POST['begin_date'], $_POST['end_date']);
-      
-      if (empty($this->errors)) {
+      if ($this->validateDateRange($_POST['begin_date'], $_POST['end_date'])) {
         
         $this->beginDate = date_create_from_format('Y-m-d', $_POST['begin_date']);
         $this->endDate = date_create_from_format('Y-m-d', $_POST['end_date']);
         unset($_POST['begin_date']);
         unset($_POST['end_date']);
+        
+      } else {
+
+        $this->redirect('/balance/show');
         
       }
     } else {
@@ -60,14 +61,24 @@ class Balance extends Authenticated
     if (cashFlow::checkDatePattern($_POST['begin_date']) == 0
           || cashFlow::checkDatePattern($_POST['end_date']) == 0) {
         
-        $this->errors[] = "Wymagany format daty RRRR-MM-DD";
+        Flash::addMessage('Wymagany format daty RRRR-MM-DD');
+        
         return false;
       }
       
       if (!cashFlow::isDateCorrect($_POST['begin_date'])
-          && !cashFlow::isDateCorrect($_POST['end_date'])) {
+          || !cashFlow::isDateCorrect($_POST['end_date'])) {
         
-        $this->errors[] = "Niepoprawna data";
+        Flash::addMessage('Niepoprawna data');
+        
+        return false;
+      }
+      
+      $begin = DateTime::createFromFormat('Y-m-d', $_POST['begin_date']);
+      $end = DateTime::createFromFormat('Y-m-d', $_POST['end_date']);
+      if ($begin->getTimestamp() > $end->getTimestamp()) {
+        Flash::addMessage('Początkowa data musi być mniejsza niż końcowa');
+        
         return false;
       }
       
