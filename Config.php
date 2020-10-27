aac0df997f3527d55e4f7af1a578688d87a15c37
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-10-27 15:49:16 +0100

    unimportant change

---
+++ Config.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'mvclogin';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = false;
+	
+	const SECRET_KEY = 'qqIjb1yUier1NJijTOar3i9EcsCY9y7X';
+}
