b101079f9008e813023be64bedcd1f1f05d2b110
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-11-12 11:26:46 +0100

    Modal showing and closing

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
   showHideDetails();
+  editAccountData();
+  hideModal();
 
 });
 
@@ -8,21 +10,36 @@ function showHideDetails() {
   
   $('.icon-dot').click(function() {
 	
-  var classKey = '.' + this.id + '-row';
-  
-  if ($(classKey).hasClass('seen')) {
+    var classKey = '.' + this.id + '-row';
     
-    $(classKey).toggleClass('item-hidden');
-    $(classKey).removeClass('seen');
-    
-  } else {
-    
-    $('.seen').toggleClass('item-hidden');
-    $('.seen').removeClass('seen');
-    $(classKey).toggleClass('item-hidden seen');
-  }
-  
-  
+    if ($(classKey).hasClass('seen')) {
+      
+      $(classKey).toggleClass('item-hidden');
+      $(classKey).removeClass('seen');
+      
+    } else {
+      
+      $('.seen').toggleClass('item-hidden');
+      $('.seen').removeClass('seen');
+      $(classKey).toggleClass('item-hidden seen');
+    }
+  });
+}
 
-});
+function editAccountData() {
+  
+  $('.account').click(function () {
+    
+    $('#updateModal').modal('toggle');
+    console.log($(this).closest('tr').attr('id'));
+    
+  });
+}
+
+function hideModal() {
+  $('#cancelModal').click(function() {
+    
+    $('#updateModal').modal('hide');
+
+  });
 }
