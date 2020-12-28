8522ea560aee1cdd1fe90a494225a3a715682d3d
Andrzej Guzowski <andrzej.guzowski.programista@gmail.com>
2020-12-28 22:30:08 +0100

    Check for existing a limit for selected category

--- public/js/spendingToMuch.js
+++ public/js/spendingToMuch.js
@@ -1,11 +1,11 @@
-var userExpensesCategories;
+var userExpensesCategories = [];
 var selectedCategory;
 
 $(document).ready(function() {
   
   loadExpensesLimits();
   checkCategorySelectedByDefault();
-  checkLimits();
+  checkSelectedCategoryByUser();
   
 });
 
@@ -15,24 +15,46 @@ function loadExpensesLimits() {
   
     function(data) {
 
-      userExpensesCategories = data;
-
+      for (i=0; i < data.length; i++) {
+        userExpensesCategories.push(data[i]);
+      }
+      
   });
-  
+
 }
 
 function checkCategorySelectedByDefault() {
   
   selectedCategory = $('#expenseCategory').val();
-  console.log('domyślna: ' + selectedCategory);
+  showLimitMessage();
+
 }
 
-function checkLimits() {
+function checkSelectedCategoryByUser() {
   
   $('#expenseCategory').change(function() {
     
     selectedCategory = $('#expenseCategory option:selected').val();
+    showLimitMessage();
 
   });
+  
+}
+
+function showLimitMessage() {
+
+  for (i = 0; i < userExpensesCategories.length; i++) {
+    
+    if (selectedCategory == userExpensesCategories[i].id) {
+      
+      if (userExpensesCategories[i].user_limit != null) {
+        
+        console.log("jest limit " + userExpensesCategories[i].user_limit);
+        
+      }
+      
+    }
+    
+  }
   
 }
